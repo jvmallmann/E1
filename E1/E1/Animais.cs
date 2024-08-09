b5d994e2e540805c3f72c3c27257d0e684da9a42
João Mallmann <joaovmsk@gmail.com>
2024-08-09 18:14:15 -0300

    Adicionar arquivos de projeto.

---
+++ E1/E1/Animais.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIVIDADES___POO.E1
+{
+    public class Animais
+    {
+        // Atributos
+        private string _nome;
+        private string _especie;
+
+        // Propriedades
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        public string Especie
+        {
+            get { return _especie; }
+            set { _especie = value; }
+        }
+
+        // Métodos
+        public void Pular()
+        {
+            Console.WriteLine($"{_nome} está pulando");
+        }
+
+        // Assinaturas
+        public void Respirar()
+        {
+            Console.WriteLine($"{_nome} está respirando");
+        }
+        public bool Respirar(string nome)
+        {
+            return true;
+        }
+
+        // Método sobrecarga
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine($"{_nome} está emitindo um som");
+        }
+
+        // Método sobrescrita
+        public virtual void Comer()
+        {
+            Console.WriteLine($"{_nome} está comendo");
+        }
+
+        // Construtor
+        public Animais(string nome, string especie)
+        {
+            _nome = nome;
+            _especie = especie;
+        }
+
+        // Classe filha (subclasse) Papagaio
+        public class Papagaio : Animais
+        {
+            private string _corDasPenas;
+            private int _idade;
+
+            public string CorDasPenas
+            {
+                get { return _corDasPenas; }
+                set { _corDasPenas = value; }
+            }
+            public int Idade
+            {
+                get { return _idade; }
+                set { _idade = value; }
+            }
+
+            public void Voar()
+            {
+                Console.WriteLine($"{Nome} está voando");
+            }
+
+            public void Falar()
+            {
+                Console.WriteLine($"{Nome} está falando");
+            }
+
+            // Construtor da subclasse
+            public Papagaio(string nome, string especie, string corDasPenas, int idade) : base(nome, especie)
+            {
+                _corDasPenas = corDasPenas;
+                _idade = idade;
+            }
+
+            // Sobrescrita de método
+            public override void EmitirSom()
+            {
+                Console.WriteLine($"{Nome} está imitando sons");
+            }
+
+            public override void Comer()
+            {
+                Console.WriteLine($"{Nome} está bicando frutas");
+            }
+        }
+    }
+}
+
---
+++ E1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ATIVIDADES___POO.E1;
+
+Animais.Papagaio papagaio = new Animais.Papagaio("Loro", "Papagaio", "Verde", 5);
+
+// Exibindo informações e chamando métodos da subclasse
+Console.WriteLine($"\nNome: {papagaio.Nome}");
+Console.WriteLine($"Espécie: {papagaio.Especie}");
+Console.WriteLine($"Cor das penas: {papagaio.CorDasPenas}");
+Console.WriteLine($"Idade: {papagaio.Idade}");
+papagaio.Pular();
+papagaio.Voar();
+papagaio.Respirar();
+papagaio.EmitirSom();
+papagaio.Comer();
